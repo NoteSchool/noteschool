95bee0a45dfb0638d625f6380b125d99cd11b63c
Aurel <lighta971@live.fr>
2013-11-05 00:33:55 +0100

    Add Test, Improve Group, Database

--- CoreLibrary/Database.cs
+++ CoreLibrary/Database.cs
@@ -5,37 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Xml.Linq;
-using System.Security.Cryptography;
 using System.Collections;
+using CoreLibrary;
 
 namespace NoteSchool
 {
-    public class Database
+    public static class Database
     {
-        private String _root = @".\saves\";
-        private String _rootNotes = @"notes\";
-        private String _groupListPath = @"groupList.xml";
+        private static String _root = @".\saves\"; //base location
+        private static String _rootNotes = @"notes\"; //location of notes
+        private static String _groupListPath = @"groupList.xml"; //group list filename
 
-        private XElement groupListElements;
+        private static XElement _groupListElements; //contain xml nodes
 
-        public Database()
+        static Database()
         {
-
-            //chemin du dossier de sauvegarde
-            //_root = Path.Combine(_root, "saves");
-
-            //chemin du dossier des groupes
+            //chemin du dossier des notes
             _rootNotes = Path.Combine(_root, _rootNotes);
 
-            //le fichier qui contient les groupes
-            _groupListPath = Path.Combine(_root, _groupListPath);
-
             //creer le dossier de sauvegarde
-            //si il n'existe pas  
             Directory.CreateDirectory(_root);
 
             //créer le dossier des notes
             Directory.CreateDirectory(_rootNotes);
+
+            #region Group Initialization
+
+            //le fichier qui contient les groupes
+            _groupListPath = Path.Combine(_root, _groupListPath);
+
 
             //create groupList file is does not exist
             if (!File.Exists(_groupListPath))
@@ -49,22 +47,25 @@ namespace NoteSchool
                 //Save the xml file
                 groupListDoc.Save(_groupListPath);
             }
+            #endregion Group Initialization
         }
 
-        public void AddGroup(Group Data)
+        #region Group Functions
+        /// <summary>
+        /// Create a new group
+        /// </summary>
+        /// <param name="Group"></param>
+        public static void AddGroup(Group Group)
         {
-            //check data integrity
-            if (Data == null || Data.Title == null)
+            //check group data integrity
+            if (string.IsNullOrEmpty(Group.Title) || Group.Hash ==  null)
             {
-                throw new ArgumentException("Invalid argument Data or Title is null");
+                throw new InvalidOperationException("You must set the group fields before call it");
             }
 
-            //Create md5 hash
-            MD5 md5hash = MD5.Create();
-            String hash = GetMd5Hash(md5hash, Data.Title + Data.Date.ToString());
-
             //create the group directory to store note
-            String groupNoteDirectory = Path.Combine(_rootNotes, hash);
+            String groupNoteDirectory = Path.Combine(_rootNotes, Group.Hash);
+
             //on créé le dossier du groupe dans le dossier notes
             //si il n'existe pas
             Directory.CreateDirectory(groupNoteDirectory);
@@ -73,36 +74,35 @@ namespace NoteSchool
             InitGroupListElements();
 
             //add the new group in the list
-            groupListElements.Add(new XElement("group",
-                new XElement("Title", Data.Title),
-                new XElement("Date", Data.Date.ToString())
-            , new XAttribute("hash", hash)));
+            _groupListElements.Add(new XElement("group",
+                new XElement("Title", Group.Title),
+                new XElement("Date", Group.Date.ToString())
+            , new XAttribute("hash", Group.Hash)));
 
             //save modification
-            groupListElements.Save(_groupListPath);
+            _groupListElements.Save(_groupListPath);
         }
 
-        public bool isGroupExists(string title)
+        public static bool isGroupExists(string title)
         {
             //load groups list file
             InitGroupListElements();
 
             //look for a group by his hash
-            IEnumerable<XElement> searched = from el in groupListElements.Elements("group")
+            IEnumerable<XElement> searched = from el in _groupListElements.Elements("group")
                                              where (string)el.Element("Title").Value == title
                                              select el;
 
             return searched.Count() > 0;
-
         }
 
-        public bool SetGroupField(String hash, String field, String value)
+        public static bool SetGroupField(String hash, String field, String value)
         {
             //load groups list file
             InitGroupListElements();
 
             //look for a group by his hash
-            IEnumerable<XElement> searched = from el in groupListElements.Elements("group")
+            IEnumerable<XElement> searched = from el in _groupListElements.Elements("group")
                                              where (string)el.Attribute("hash") == hash
                                              select el;
 
@@ -115,18 +115,18 @@ namespace NoteSchool
             elem.SetElementValue(field, value);
 
             //save modification
-            groupListElements.Save(_groupListPath);
+            _groupListElements.Save(_groupListPath);
 
             return true;
         }
 
-        public bool GetGroupByHash(String hash, Group Group)
+        public static bool GetGroupByHash(String hash, Group Group)
         {
             //load groups list file
             InitGroupListElements();
 
             //look for a group by his hash
-            IEnumerable<XElement> searched = from el in groupListElements.Elements("group")
+            IEnumerable<XElement> searched = from el in _groupListElements.Elements("group")
                                              where (string)el.Attribute("hash") == hash
                                              select el;
 
@@ -144,40 +144,20 @@ namespace NoteSchool
 
             XElement elem = searched.First();
             Group.Title = elem.Element("Title").Value;
+           // Group.Date = (DateTime) elem.Element("Date");
 
             return true;
         }
 
-        private void InitGroupListElements()
+        private static void InitGroupListElements()
         {
             //init groupListDoc if not yet
-            if (groupListElements == null)
+            if (_groupListElements == null)
             {
-                groupListElements = XElement.Load(_groupListPath);
+                _groupListElements = XElement.Load(_groupListPath);
             }
         }
-
-        private string GetMd5Hash(MD5 md5Hash, string input)
-        {
-
-            // Convert the input string to a byte array and compute the hash.
-            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-            // Create a new Stringbuilder to collect the bytes
-            // and create a string.
-            StringBuilder sBuilder = new StringBuilder();
-
-            // Loop through each byte of the hashed data 
-            // and format each one as a hexadecimal string.
-            for (int i = 0; i < data.Length; i++)
-            {
-                sBuilder.Append(data[i].ToString("x2"));
-            }
-
-            // Return the hexadecimal string.
-            return sBuilder.ToString();
-        }
-
-        internal Group Null { get; set; }
     }
+
+        #endregion Group Functions
 }
